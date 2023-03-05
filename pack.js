815705a8554f1ab0e60092a379b8daba8feed7c8
Louis Poirier <contact@louispoirier.io>
2023-03-05 20:32:38 +0100

    build: improve script 'pack.js' to prepare release archive.

--- pack.js
+++ pack.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
 
-function main() {
+async function main() {
     const packageFile = fs.readFileSync('package.json', {encoding: 'utf8', flag: 'r'});
     const packageJson = JSON.parse(packageFile);
 
@@ -13,6 +15,32 @@ function main() {
     fs.writeFileSync('dist/package.json', JSON.stringify(packageJson), {encoding: 'utf8', flag: 'w'});
     fs.copyFileSync('README.md', 'dist/README.md');
     fs.copyFileSync('LICENSE', 'dist/LICENSE');
+
+    if (fs.existsSync('dist/schemas')) {
+        fs.rmSync('dist/schemas', {recursive: true, force: true});
+    }
+
+    if (!fs.existsSync('dist/src')) {
+        return;
+    }
+    const sections = ["main", "preload", "renderer"];
+
+    for (const section of sections) {
+        const files = await glob(`dist/src/${section}/*.ts`);
+
+        files.forEach((file) => {
+            fs.renameSync(file, path.join(`dist/${section}`, path.basename(file)));
+        });
+    }
+    const files = await glob('dist/src/*.ts');
+
+    files.forEach((file) => {
+        fs.renameSync(file, path.join(`dist`, path.basename(file)));
+    });
+
+    fs.rmSync('dist/src', {recursive: true, force: true});
 }
 
-main();
+(async () => {
+    await main();
+})();
